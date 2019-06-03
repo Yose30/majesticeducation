9e832de2c435cb3153f09130ee1d4bcdb98fd375
zoso722 <canalestomas722@hotmail.com>
2019-06-03 17:49:12 -0500

    ADD M

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::get('/contenido/{id}', 'LibroController@contenido')->name('contenido');
 
 
 Route::get('/download/{id}', 'LibroController@download')->name('download');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
